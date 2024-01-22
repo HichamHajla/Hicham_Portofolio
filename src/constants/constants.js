de25084ffc0037eec380625bd47d84f092b39fb0
HichamHajla <hichem500@hotmail.com>
2024-01-22 15:35:36 +0100

    new

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,24 +8,24 @@ export const projects = [
     visit: 'https://wall-ai-front.vercel.app/',
     id: 0,
   },
-  {
-    title: 'Pokemon Battle',
-    description:"A 3 days React project of a Pokemon battle game. You chose a Pokemon in the Pokédex then you have to fight the opponent Pokemon which is randomly chosen by the app. (Press A to fight)",
-    image: '/images/Battle.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://pokemon-battle-mu.vercel.app/',
-    id: 1,
-  },
-  {
-    title: 'The Poke-Mem',
-    description: "My first project at ironhack, a mini-game build using Document Object Model (DOM). It's a memory card game where you have to memorize and find all the pairs. You won the game once you found all the pairs of cards. (4 levels of difficulty)",
-      image: '/images/pokemem.jpeg',
-      tags: ['Javascript', 'HTML', 'DOM'],
-    source: 'https://google.com',
-    visit: 'https://mini-game-project-six.vercel.app/',
-    id: 2,
-  },
+  // {
+  //   title: 'Pokemon Battle',
+  //   description:"A 3 days React project of a Pokemon battle game. You chose a Pokemon in the Pokédex then you have to fight the opponent Pokemon which is randomly chosen by the app. (Press A to fight)",
+  //   image: '/images/Battle.png',
+  //   tags: ['React', 'JavaScript'],
+  //   source: 'https://google.com',
+  //   visit: 'https://pokemon-battle-mu.vercel.app/',
+  //   id: 1,
+  // },
+  // {
+  //   title: 'The Poke-Mem',
+  //   description: "My first project at ironhack, a mini-game build using Document Object Model (DOM). It's a memory card game where you have to memorize and find all the pairs. You won the game once you found all the pairs of cards. (4 levels of difficulty)",
+  //     image: '/images/pokemem.jpeg',
+  //     tags: ['Javascript', 'HTML', 'DOM'],
+  //   source: 'https://google.com',
+  //   visit: 'https://mini-game-project-six.vercel.app/',
+  //   id: 2,
+  // },
 ];
 
 export const TimeLineData = [
